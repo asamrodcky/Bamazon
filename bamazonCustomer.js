974c57c594a68f5687b666fefdaff7ff0f61dd57
samrod826 <samrodriguez826@gmail.com>
2019-12-06 19:08:56 -0500

    displays easy-table in terminal cleanly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("easy-table");
 
 // Connection credentials
 var connection = mysql.createConnection({
@@ -14,15 +15,39 @@ connection.connect(function (err) {
     runSearch();
 })
 
-function runSearch(){
+function runSearch() {
     console.log("This is running")
+    var query = "SELECT * from products"
+    var items = []
+    query = connection.query(query, function (err, res) {
+        if (err) throw err;
+        // console.log(res)
+        for(var i = 0; i < res.length;i++){
+            items.push(res[i].product_name)
+        }
+        console.log(items)
+        var t = new Table
+
+        res.forEach(function (product) {
+            t.cell('Item ID', product.item_id)
+            t.cell('Product Name', product.product_name)
+            t.cell('Price, USD', product.price, Table.number(2))
+            t.cell('Stock Quantity', product.stock_quanitity)
+            t.newRow()
+        })
+
+        console.log(t.toString())
+    })
     // inquirer
     //     .prompt([
     //         {
-
+    //             type: "list",
+    //             message: "What item would you like to buy",
+    //             choices: [JSON.stringify(items.map())],
+    //             name: "itemToBuy"
     //         }
     //     ])
     //     .then(function(response){
-
+    //         console.log("This works?")
     //     })
 }
